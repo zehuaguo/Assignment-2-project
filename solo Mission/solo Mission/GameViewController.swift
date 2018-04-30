55bc9b54c8c28fab9c66b72cb8fc15ae623c15b8
zehua guo <zehuaguo@zehuademacbook-pro.local>
2018-04-30 23:49:44 +1000

    add background, player plane, bullet, and fire bullet sound

--- solo Mission/solo Mission/GameViewController.swift
+++ solo Mission/solo Mission/GameViewController.swift
@@ -17,29 +17,17 @@ class GameViewController: UIViewController {
         //jintianzhenshigehaotianqi
         // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
         // including entities and graphs.
-        if let scene = GKScene(fileNamed: "GameScene") {
+        let scene = GameScene(size: CGSize (width: 1536, height: 2048))
             
             // Get the SKScene from the loaded GKScene
-            if let sceneNode = scene.rootNode as! GameScene? {
-                
-                // Copy gameplay related content over to the scene
-                sceneNode.entities = scene.entities
-                sceneNode.graphs = scene.graphs
-                
-                // Set the scale mode to scale to fit the window
-                sceneNode.scaleMode = .aspectFill
-                
-                // Present the scene
-                if let view = self.view as! SKView? {
-                    view.presentScene(sceneNode)
-                    
-                    view.ignoresSiblingOrder = true
-                    
-                    view.showsFPS = true
-                    view.showsNodeCount = true
-                }
-            }
-        }
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+        
+        
     }
 
     override var shouldAutorotate: Bool {
